9d3b93c56f5032f3e6380e12da030dc1def387bf
gandilong <gandilong@yeah.net>
2013-04-03 10:06:54 +0800

    some update
    ModelSQL 第一版己正式构建完成。

--- src/org/com/thang/model/mate/SQLMate.java
+++
@@ -1,9 +0,0 @@
-package org.com.thang.model.mate;
-
-import org.com.thang.model.sql.Link;
-
-public @interface SQLMate {
-
-	public Link Mate() default Link.EQUAL;
-	
-}
--- src/org/com/thang/processor/StringProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.com.thang.processor;
-
-public interface StringProcessor {
-
-	public String processor(String str);
-	
-}
